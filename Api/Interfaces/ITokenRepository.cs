af0d5c2baef11cc6290ab88f98ddcd90c013dad0
Ahsm123 <10503096@ucn.dk>
2025-06-01 16:06:54 +0200

    Add Interfaces for Token

---
+++ Api/Interfaces/ITokenRepository.cs
@@ -0,0 +1,16 @@
+﻿using Api.Models;
+
+namespace Api.Interfaces;
+
+public interface ITokenRepository
+{
+    Task<Token> CreateAsync(Token token);
+    Task<Token?> GetValidTokenAsync(string tokenString, TokenType type, Guid userId);
+    Task<Token?> GetByTokenStringAsync(string tokenString);
+    Task<Token> UpdateAsync(Token token);
+    Task DeleteAsync(Token token);
+    Task<IEnumerable<Token>> GetUserTokensAsync(Guid userId, TokenType? type = null);
+    Task BlacklistTokenAsync(string tokenString);
+    Task BlacklistUserTokensAsync(Guid userId, TokenType? type = null);
+    Task DeleteExpiredTokensAsync();
+}
---
+++ Api/Interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using Api.Dtos.TokenDtos;
+using Api.Models;
+
+namespace Api.Interfaces;
+
+public interface ITokenService
+{
+    Task<AuthTokensDto> GenerateAuthTokensAsync(User user);
+    Task<AuthTokensDto> RefreshTokensAsync(string refreshToken);
+    Task<string> GenerateResetPasswordTokenAsync(string email);
+    Task<string> GenerateVerifyEmailTokenAsync(User user);
+    Task<Token> ValidateTokenAsync(string token, TokenType expectedType);
+    Task RevokeTokenAsync(string token);
+    Task RevokeUserTokensAsync(Guid userId, TokenType? type = null);
+    Task CleanupExpiredTokensAsync();
+}
